cfb0562593b3a5fa505fec1bb50ce064619801f2
Prathamesh Salunkhe <spratham55@gmail.com>
2021-12-17 21:54:28 +0530

    Solve day 5 puzzle part 2

--- day-05/main.go
+++ day-05/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -27,8 +28,9 @@ func main() {
 		log.Fatal(err)
 	}
 
-	overlapMap := make(map[string]int)
-	trackOverlappingPoints := 0
+	overlapMap := make(map[string][]int)
+	trackOverlappingPointsAns1 := 0
+	trackOverlappingPointsAns2 := 0
 
 	for _, line := range input {
 		if line.p1.y == line.p2.y {
@@ -36,34 +38,73 @@ func main() {
 			for i := line.p1.x; i <= line.p2.x; i++ {
 				pointStr := strconv.Itoa(i) + "," + strconv.Itoa(line.p1.y)
 
-				if trackOverlaps(overlapMap, pointStr) {
-					trackOverlappingPoints++
+				if trackOverlapsForAnswer1(overlapMap, pointStr) {
+					trackOverlappingPointsAns1++
+				}
+
+				if trackOverlapsForAnswer2(overlapMap, pointStr) {
+					trackOverlappingPointsAns2++
 				}
 			}
-		} else {
+		} else if line.p1.x == line.p2.x {
 			// vertical line
 			for i := line.p1.y; i <= line.p2.y; i++ {
 				pointStr := strconv.Itoa(line.p1.x) + "," + strconv.Itoa(i)
 
-				if trackOverlaps(overlapMap, pointStr) {
-					trackOverlappingPoints++
+				if trackOverlapsForAnswer1(overlapMap, pointStr) {
+					trackOverlappingPointsAns1++
+				}
+
+				if trackOverlapsForAnswer2(overlapMap, pointStr) {
+					trackOverlappingPointsAns2++
+				}
+			}
+		} else {
+			// diagonal line
+			for i := line.p1.x; i <= line.p2.x; i++ {
+				isDiagonalClimbing := line.p1.y < line.p2.y
+
+				pointY := line.p1.y
+				if isDiagonalClimbing {
+					pointY += (i - line.p1.x)
+				} else {
+					pointY -= (i - line.p1.x)
+				}
+
+				pointStr := strconv.Itoa(i) + "," + strconv.Itoa(pointY)
+
+				if trackOverlapsForAnswer2(overlapMap, pointStr) {
+					trackOverlappingPointsAns2++
 				}
 			}
 		}
 	}
 
-	answer1 := trackOverlappingPoints
+	answer1 := trackOverlappingPointsAns1
 	log.Printf("Answer #1 :: %d", answer1)
+
+	answer2 := trackOverlappingPointsAns2
+	log.Printf("Answer #2 :: %d", answer2)
 }
 
-func trackOverlaps(overlapMap map[string]int, pointStr string) bool {
+func trackOverlapsForAnswer1(overlapMap map[string][]int, pointStr string) bool {
 	if _, ok := overlapMap[pointStr]; !ok {
-		overlapMap[pointStr] = 0
+		overlapMap[pointStr] = []int{0, 0}
 	}
 
-	overlapMap[pointStr] += 1
+	overlapMap[pointStr][0] += 1
 
-	return overlapMap[pointStr] == 2
+	return overlapMap[pointStr][0] == 2
+}
+
+func trackOverlapsForAnswer2(overlapMap map[string][]int, pointStr string) bool {
+	if _, ok := overlapMap[pointStr]; !ok {
+		overlapMap[pointStr] = []int{0, 0}
+	}
+
+	overlapMap[pointStr][1] += 1
+
+	return overlapMap[pointStr][1] == 2
 }
 
 func getInput() ([]Line, error) {
@@ -87,7 +128,22 @@ func getInput() ([]Line, error) {
 
 		var line Line
 
-		if p1.y == p2.y {
+		// TODO can be refactored
+		if isLineDiagonal(p1, p2) {
+			if p1.x < p2.x {
+				line = Line{
+					p1: p1,
+					p2: p2,
+				}
+			} else {
+				line = Line{
+					p1: p2,
+					p2: p1,
+				}
+			}
+
+			input = append(input, line)
+		} else if p1.y == p2.y {
 			// horizontal line
 			if p1.x < p2.x {
 				line = Line{
@@ -115,12 +171,17 @@ func getInput() ([]Line, error) {
 					p2: p1,
 				}
 			}
+
 			input = append(input, line)
 		}
 
 	}
 
 	return input, scanner.Err()
+}
+
+func isLineDiagonal(p1 Point, p2 Point) bool {
+	return math.Abs(float64(p2.x-p1.x)) == math.Abs(float64(p2.y-p1.y))
 }
 
 func convertStrToPoint(pointStr string) Point {
